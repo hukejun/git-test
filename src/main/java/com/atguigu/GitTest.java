8cf77a111b3ae7b07393f1c7eff32eec4add5471
hukejun <18372090687@163.com>
2023-02-10 22:35:44 +0800

    第一个提交

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+/*
+ * @Author: hu_jun
+ * @Date: 2023/02/10/22:32
+ */
+
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
